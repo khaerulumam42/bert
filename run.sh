7bc9b619fa75a60427919285215107406662196a
Khaerul Umam <umam@sdtech.co.id>
2019-07-10 11:05:20 +0700

    add some folder to gitignore

---
+++ run.sh
@@ -0,0 +1,1 @@
+python run_squad.py --train_file dataset/tmp_train_lenna_v2.0.json --predict_file dataset/tmp_dev_lenna_v2.0.json --do_lower_case --do_train --do_predict --vocab_file bert-base-multilingual-cased-vocab.txt --bert_config_file bert_config.json --output_dir squad_id --use_tpu --tpu_name tpuv3-big
